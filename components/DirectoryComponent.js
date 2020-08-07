f5b33c52c6c267d0e49a19f8e9bfa69cbfb3bb97
Rashid Goshtasbi <rashid.goshtasbi@gmail.com>
2020-08-06 23:03:28 -0700

    added more components

--- components/DirectoryComponent.js
+++ components/DirectoryComponent.js
@@ -1,32 +1,26 @@
-import React, { Component } from "react";
-import { Text, View } from "react-native";
+import React from "react";
 import { FlatList } from "react-native";
 import { ListItem } from "react-native-elements";
 
-export class DirectoryComponent extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    const renderDirectoryItem = ({ item }) => {
-      return (
-        <ListItem
-          title={item.name}
-          subtitle={item.description}
-          leftAvatar={{ source: require("./images/react-lake.jpg") }}
-        />
-      );
-    };
-
+function Directory(props) {
+  const renderDirectoryItem = ({ item }) => {
     return (
-      <FlatList
-        data={this.props.campsites}
-        renderItem={renderDirectoryItem}
-        keyExtractor={(item) => item.id.toString()}
+      <ListItem
+        title={item.name}
+        subtitle={item.description}
+        onPress={() => props.onPress(item.id)}
+        leftAvatar={{ source: require("./images/react-lake.jpg") }}
       />
     );
-  }
+  };
+
+  return (
+    <FlatList
+      data={props.campsites}
+      renderItem={renderDirectoryItem}
+      keyExtractor={(item) => item.id.toString()}
+    />
+  );
 }
 
-export default DirectoryComponent;
+export default Directory;
--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,35 +1,64 @@
 import React, { Component } from "react";
 import { Text, View, StyleSheet, SafeAreaView, ScrollView } from "react-native";
-import { CAMPSITES } from "../shared/campsites";
 import DirectoryComponent from "./DirectoryComponent";
+import CampsiteInfoComponent from "./CampsiteInfoComponent";
+import { CAMPSITES } from "../shared/campsites";
 
-export class MainComponent extends Component {
+class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       campsites: CAMPSITES,
+      selectedCampsite: null,
     };
+  }
+
+  onCampsiteSelect(campsiteId) {
+    this.setState({ selectedCampsite: campsiteId });
   }
 
   render() {
     return (
-      // <ScrollView contentInsetAdjustmentBehavior="automatic">
-      //   <SafeAreaView />
-      <DirectoryComponent campsites={this.state.campsites} />
-      //{/* </SafeAreaView> */}
-      //</ScrollView>
-      // <View>
-      //   <SafeAreaView />
-      //   <DirectoryComponent campsites={this.state.campsites} />
-      // </View>
+      <View style={{ flex: 1 }}>
+        <DirectoryComponent
+          campsites={this.state.campsites}
+          onPress={(campsiteId) => this.onCampsiteSelect(campsiteId)}
+        />
+        <CampsiteInfoComponent
+          campsite={
+            this.state.campsites.filter(
+              (campsite) => campsite.id === this.state.selectedCampsite
+            )[0]
+          }
+        />
+      </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  safeArea: {
-    paddingTop: Platform.OS === "android" ? 25 : 0,
-  },
-});
+export default Main;
 
-export default MainComponent;
+//       // <ScrollView contentInsetAdjustmentBehavior="automatic">
+//       //   <SafeAreaView />
+//       <View style={{ flex: 1 }}>
+//         <DirectoryComponent
+//           campsites={this.state.campsites}
+//           onPress={(campsiteId) => this.onCampsiteSelect(campsiteId)}
+//         />
+//         <CampsiteInfoComponent
+//           campsite={
+//             this.state.campsites.filter(
+//               (campsite) => campsite.id === this.state.selectedCampsite
+//             )[0]
+//           }
+//         />
+//       </View>
+//       //{/* </SafeAreaView> */}
+//       //</ScrollView>
+//       // <View>
+//       //   <SafeAreaView />
+//       //   <DirectoryComponent campsites={this.state.campsites} />
+//       // </View>
+//     );
+//   }
+// }
