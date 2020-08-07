dbc027410fb59468c2c2a58f501c5f4dc4fe0249
Rashid Goshtasbi <rashid.goshtasbi@gmail.com>
2020-08-06 23:47:20 -0700

    working part 1 navigation

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,64 +1,42 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet, SafeAreaView, ScrollView } from "react-native";
-import DirectoryComponent from "./DirectoryComponent";
-import CampsiteInfoComponent from "./CampsiteInfoComponent";
-import { CAMPSITES } from "../shared/campsites";
+import { View, Platform } from "react-native";
+import { createStackNavigator } from "react-navigation";
+import Directory from "./DirectoryComponent";
+import CampsiteInfo from "./CampsiteInfoComponent";
+
+const DirectoryNavigator = createStackNavigator(
+  {
+    Directory: { screen: Directory },
+    CampsiteInfo: { screen: CampsiteInfo },
+  },
+  {
+    initialRouteName: "Directory",
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: "#5637DD",
+      },
+      headerTintColor: "#fff",
+      headerTitleStyle: {
+        color: "#fff",
+      },
+    },
+  }
+);
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      campsites: CAMPSITES,
-      selectedCampsite: null,
-    };
-  }
-
-  onCampsiteSelect(campsiteId) {
-    this.setState({ selectedCampsite: campsiteId });
-  }
-
   render() {
     return (
-      <View style={{ flex: 1 }}>
-        <DirectoryComponent
-          campsites={this.state.campsites}
-          onPress={(campsiteId) => this.onCampsiteSelect(campsiteId)}
-        />
-        <CampsiteInfoComponent
-          campsite={
-            this.state.campsites.filter(
-              (campsite) => campsite.id === this.state.selectedCampsite
-            )[0]
-          }
-        />
+      <View
+        style={{
+          flex: 1,
+          paddingTop:
+            Platform.OS === "ios" ? 0 : Expo.Constants.statusBarHeight,
+        }}
+      >
+        <DirectoryNavigator />
       </View>
     );
   }
 }
 
 export default Main;
-
-//       // <ScrollView contentInsetAdjustmentBehavior="automatic">
-//       //   <SafeAreaView />
-//       <View style={{ flex: 1 }}>
-//         <DirectoryComponent
-//           campsites={this.state.campsites}
-//           onPress={(campsiteId) => this.onCampsiteSelect(campsiteId)}
-//         />
-//         <CampsiteInfoComponent
-//           campsite={
-//             this.state.campsites.filter(
-//               (campsite) => campsite.id === this.state.selectedCampsite
-//             )[0]
-//           }
-//         />
-//       </View>
-//       //{/* </SafeAreaView> */}
-//       //</ScrollView>
-//       // <View>
-//       //   <SafeAreaView />
-//       //   <DirectoryComponent campsites={this.state.campsites} />
-//       // </View>
-//     );
-//   }
-// }
